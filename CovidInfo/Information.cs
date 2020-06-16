7d43908581a35fb9133cb4b4b880e8d84b2199ae
Cryental <44664655+Cryental@users.noreply.github.com>
2020-06-16 12:08:53 +0600

    Added access_key parameter support

--- CovidInfo/Information.cs
+++ CovidInfo/Information.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Net;
 using Covid.Models;
 using Newtonsoft.Json;
@@ -7,40 +7,87 @@ namespace Covid
 {
     public class Information
     {
+        private readonly string _accessKey;
+
+        public Information()
+        {
+        }
+
+        public Information(string accessKey)
+        {
+            _accessKey = accessKey;
+        }
+
         public Latest GetCounters()
         {
             var url = "https://cryental.dev/api/covid-status/latest";
 
-            var webclient = new WebClient();
-            var result = webclient.DownloadString(url);
+            if (string.IsNullOrEmpty(_accessKey))
+            {
+                url += $"?access_key={_accessKey}";
+            }
 
-            var parsedObject = JsonConvert.DeserializeObject<LatestModel>(result);
+            var webClient = new WebClient();
+            var result = webClient.DownloadString(url);
 
-            return parsedObject.Latest;
+            try
+            {
+                var parsedObject = JsonConvert.DeserializeObject<LatestModel>(result);
+
+                return parsedObject.Latest;
+            }
+            catch
+            {
+                throw new WebException("The access key is invalid.");
+            }
         }
 
         public List<Location> GetAllLocations()
         {
             var url = "https://cryental.dev/api/covid-status/locations";
 
-            var webclient = new WebClient();
-            var result = webclient.DownloadString(url);
+            if (string.IsNullOrEmpty(_accessKey))
+            {
+                url += $"?access_key={_accessKey}";
+            }
 
-            var parsedObject = JsonConvert.DeserializeObject<LocationsModel>(result);
+            var webClient = new WebClient();
+            var result = webClient.DownloadString(url);
 
-            return parsedObject.Locations;
+            try
+            {
+                var parsedObject = JsonConvert.DeserializeObject<LocationsModel>(result);
+
+                return parsedObject.Locations;
+            }
+            catch
+            {
+                throw new WebException("The access key is invalid.");
+            }
         }
 
         public List<Location> GetByLocation(Countries country)
         {
             var url = "https://cryental.dev/api/covid-status/locations?country=" + country;
 
-            var webclient = new WebClient();
-            var result = webclient.DownloadString(url);
+            if (string.IsNullOrEmpty(_accessKey))
+            {
+                url += $"&access_key={_accessKey}";
+            }
 
-            var parsedObject = JsonConvert.DeserializeObject<LocationsModel>(result);
+            var webClient = new WebClient();
+            var result = webClient.DownloadString(url);
 
-            return parsedObject.Locations;
+            try
+            {
+                var parsedObject = JsonConvert.DeserializeObject<LocationsModel>(result);
+
+                return parsedObject.Locations;
+            }
+            catch
+            {
+                throw new WebException("The access key is invalid.");
+            }
         }
     }
 }
